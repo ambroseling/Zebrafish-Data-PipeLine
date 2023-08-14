441ed29949f75781fbfc562ee7e14cd079f276bb
Ambrose Ling <lingambrose@gmail.com>
2023-08-14 15:43:52 -0400

    backend changes

--- zebrafish-data-pipeline-be/app.py
+++ zebrafish-data-pipeline-be/app.py
@@ -1,15 +1,30 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify,make_response
+
 import os, base64
 from flask_cors import CORS  # Import the CORS extension
 
+import pandas as pd
 
+import tempfile
 
 import cv2
 import tempfile
+import json
 
 app = Flask(__name__)
 
 CORS(app) 
+@app.route('/save_json', methods=['POST'])
+def save_json():
+    try:
+        data = request.get_json()  # Get JSON data from the request
+        # Save the JSON data to a file
+        with open('data.json', 'w') as file:
+            file.write(json.dumps(data, indent=4))
+        return jsonify({'message': 'JSON data saved successfully.'}), 200
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
 @app.route('/extract_images', methods=['POST'])
 def extract_images():
     # Get the uploaded videos from the request
@@ -17,8 +32,6 @@ def extract_images():
     #print("RECEIVED DATA: ",data)
     data_path =  "/Users/ambroseling/Desktop/zebrafish-data-pipeline/data"
     uploaded_files = request.files.getlist('videos')
-    print("UPLOADED FILES:")
-    print(uploaded_files)
     # Sample rate (number of frames to skip between each extracted frame)
     print("SAMPLE RATE:")
     sample_rate = int(request.form.get('sample_rate', 10))
@@ -75,6 +88,49 @@ def get_images():
 
     # Return the list of base64-encoded image data in the JSON response
     return jsonify({'images': image_data_list})
+
+header = [('scorer','' , '', 'experimenter', 'experimenter', 'experimenter', 'experimenter', 'experimenter', 'experimenter', 'experimenter', 'experimenter', 'experimenter', 'experimenter', 'experimenter', 'experimenter', 'experimenter', 'experimenter', 'experimenter', 'experimenter', 'experimenter', 'experimenter', 'experimenter'),
+            ('bodyparts', '', '', 'head', 'head', 'tail_1', 'tail_1', 'tail_2', 'tail_2', 'tail_3', 'tail_3', 'tail_4', 'tail_4', 'tail_5', 'tail_5', 'tail_6', 'tail_6', 'tail_7', 'tail_7', 'tail_8', 'tail_8', 'tail_9', 'tail_9'),
+            ('coords', '', '', 'x', 'y', 'x', 'y', 'x', 'y', 'x', 'y', 'x', 'y', 'x', 'y', 'x', 'y', 'x', 'y', 'x', 'y', 'x', 'y')]
+
+CORS(app) 
+@app.route('/get_csv', methods=['POST'])
+def generate_csv():
+    data = []
+    data.extend(header)
+
+    coordinate_data = request.get_json()
+    for video in coordinate_data:
+        for image in coordinate_data[video]:
+            data_entry = ['labeled-data', video]
+            data_entry.append(image['path'])
+            coordinates = image['coordinates']
+
+            i = 0
+            while i < min(10, len(coordinates)):
+                data_entry.append(coordinates[i]["x"])
+                data_entry.append(coordinates[i]["y"])
+                i += 1
+            while i < 10:
+                data_entry.append('')
+                i += 1
+            
+            data.append(data_entry)
+
+
+    df = pd.DataFrame(data)
+    resp = make_response(df.to_csv(index=False, header=False))
+    resp.headers["Content-Disposition"] = "attachment; filename=export.csv"
+    resp.headers["Content-Type"] = "text/csv"
+    return resp
+
+
+# @app.route('/get_images', methods=['GET'])
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
